1f42387335357482ac5d2473e7106eccafe09a6b
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-07-02 10:23:06 +1200

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "example2" {
   name     = "my-resource-group"
   location = "australiaeast"  # Replace with your desired Azure region
 }
