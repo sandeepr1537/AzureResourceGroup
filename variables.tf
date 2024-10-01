47a1a10c86bee95308b4a3e22569b4cfa0d2c3ee
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-10-02 08:20:40 +1300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,2 @@
 # variables.tf
 
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
- 
-}
-
-variable "location" {
-  description = "The location of the resource group"
-  type        = string
-
-}
