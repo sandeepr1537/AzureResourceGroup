4e9958b95e90244e2f86321ff02f708c531afd09
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-10-02 09:35:55 +1300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,6 @@
-# providers.tf
 provider "azurerm" {
   features {}
 
   subscription_id = "b2cab71b-eb34-4062-9a43-dc2d606ec98f"
-  tenant_Id": "15f3c739-5bdb-40b6-b071-94560fc2efd4"
+  tenant_id       = "15f3c739-5bdb-40b6-b071-94560fc2efd4"  # Corrected line
 }
