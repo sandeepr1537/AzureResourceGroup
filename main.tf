97e0237823e18b22415d2e3e1ebbc76624c15f46
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-10-02 08:30:59 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
+# main.tf
 resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "Australia East"
-}
+  name     = var.resource_group_name
+  location = var.location
 
+  tags = {
+    ServiceName  = "MyService"
+    ServiceOwner = "YourName"
+    Environment  = "Development"
+  }
+}
