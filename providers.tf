e3692f7947d5dfa7bb0b74e69a0902fa4d99e13e
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2024-10-02 08:30:40 +1300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.3.0"
-    }
-  }
-}
-
+# providers.tf
 provider "azurerm" {
   features {}
-
- subscription_id = "b2cab71b-eb34-4062-9a43-dc2d606ec98f"
-
 }
